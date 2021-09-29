c92b187d2ff698f76896f6bee94540971d86c95b
anu2319 <anukumaravel8@gmail.com>
2021-09-29 21:28:57 +0530

    added some files

---
+++ Declare Variable and print those values/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int num = 5;
+        char ch = 'A';
+        float value = 6.23f;
+        double d = 20.01d;
+        long l = 345267L;
+        boolean b = true;
+        System.out.println("Int : " + num);
+        System.out.println("Char : " + ch);
+        System.out.println("Float : " + value);
+        System.out.println("Double : " + d);
+        System.out.println("Long : " + l);
+        System.out.println("Boolean : " + b);
+        System.out.println("Java does not have a datatype for unsigned integers and unsigned float");
+
+    }
+}
---
+++ print the values in output screen/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println(65);
+        System.out.println("\'A\'");
+        System.out.println(12.05);
+        System.out.println("-0.0000034");
+        System.out.println("\"ACGCET\"");
+        System.out.println(-127);
+        System.out.println("\"WELCOME TO ACGCET\"");
+        System.out.println(2021);
+    }
+}
